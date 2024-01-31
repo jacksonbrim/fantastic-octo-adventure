37c44b329f04a0d0da0663d755cd849dbe2743b9
Jackson Brim <jackson.cr.brim@gmail.com>
2024-01-31 14:30:38 -0600

    add unit conversion to cli with positional args, refactor unit_conversion.rs to conversion.rs, create public function ConversionQuery::convert() to handle conversion

--- src/cli.rs
+++ src/cli.rs
@@ -1,15 +1,13 @@
 use clap::{Arg, ArgAction, ArgMatches, Command};
-
-pub fn parse_args() -> (i64, Option<String>, u8) {
-    let matches = Command::new("Dice Game Simulations")
+pub fn parse_args() -> (i64, Option<String>, u8, Option<(f32, String, String)>) {
+    let matches = Command::new("Dice Game Simulations & Unit Conversion")
         .author("Jackson Brim")
         .version("0.1.0")
-        .about("Dice Game Simulations")
+        .about("Dice Game Simulation & Unit Conversion")
         .arg(
             Arg::new("num-sims")
                 .short('n')
                 .long("num-sims")
-                .required(true)
                 .value_parser(clap::value_parser!(i64))
                 .help("The number of simulations to run."),
         )
@@ -19,6 +17,38 @@ pub fn parse_args() -> (i64, Option<String>, u8) {
                 .long("output")
                 .action(ArgAction::Set)
                 .help("Specify the output file for generated numbers"),
+        )
+        .arg(
+            Arg::new("unit-conversion")
+                .short('u')
+                .long("unit-conversion")
+                .action(ArgAction::SetTrue)
+                .help("Perform a unit conversion, requires -v <value> -f <from> -t <to>"),
+        )
+        .arg(
+            Arg::new("value")
+                .index(1)
+                .value_parser(clap::value_parser!(f32))
+                .requires("unit-conversion")
+                .help("The value to convert"),
+        )
+        .arg(
+            Arg::new("from")
+                .index(2)
+                .value_parser(clap::builder::ValueParser::new(
+                    clap::builder::PossibleValuesParser::new(["m", "in", "ft", "hr", "s"]),
+                ))
+                .requires("unit-conversion")
+                .help("The unit to convert from ('m', 'in', 'ft', 'hr', 's')"),
+        )
+        .arg(
+            Arg::new("to")
+                .index(3)
+                .value_parser(clap::builder::ValueParser::new(
+                    clap::builder::PossibleValuesParser::new(["m", "in", "ft", "hr", "s"]),
+                ))
+                .requires("unit-conversion")
+                .help("The unit to convert to ('m', 'in', 'ft', 'hr', 's')"),
         )
         .arg(
             Arg::new("verbose")
@@ -38,6 +68,15 @@ pub fn parse_args() -> (i64, Option<String>, u8) {
     let num_sims = *matches.get_one::<i64>("num-sims").unwrap_or(&0);
     let output = matches.get_one::<String>("output").cloned();
     let verbosity = *matches.get_one::<u8>("verbose").unwrap_or(&0);
+    let unit_conversion_match = *matches.get_one::<bool>("unit-conversion").unwrap_or(&false);
+    let unit_conversion = if unit_conversion_match {
+        let value = *matches.get_one::<f32>("value").unwrap();
+        let from = matches.get_one::<String>("from").cloned().unwrap();
+        let to = matches.get_one::<String>("to").cloned().unwrap();
+        Some((value, from, to))
+    } else {
+        None
+    };
 
-    (num_sims, output, verbosity)
+    (num_sims, output, verbosity, unit_conversion)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod cli;
+mod conversion;
 mod dice_game;
 mod expected_values;
 mod game;
-mod unit_conversion;
 
+use crate::conversion::unit_conversion;
 use cli::parse_args;
 use std::error::Error;
 use tracing::{info, level_filters::LevelFilter};
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let (num_sims, output, verbosity) = parse_args();
+    let (num_sims, output, verbosity, unit_conversion) = parse_args();
 
     setup_logging(verbosity);
     info!("verbosity level: {:?}", verbosity);
@@ -17,6 +17,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     if num_sims > 0 {
         let output_fp = dice_game::simulate_dice_games(num_sims, output.as_deref())?;
         println!("Output written to file: {}", output_fp);
+    }
+    if let Some((value, from, to)) = unit_conversion {
+        let result = unit_conversion::ConversionQuery::convert(value, &from, &to);
+        match result {
+            Ok(converted_value) => println!("Converted value: {}", converted_value),
+            Err(e) => eprintln!("Conversion error: {}", e),
+        }
     }
 
     Ok(())
